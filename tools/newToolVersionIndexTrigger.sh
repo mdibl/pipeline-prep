ff566db2d70ba9b9d95a7edac761f779827d8aa7
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-02-01 14:11:20 -0500

    Update newToolVersionIndexTrigger.sh

--- tools/newToolVersionIndexTrigger.sh
+++ tools/newToolVersionIndexTrigger.sh
@@ -12,62 +12,46 @@ cd `dirname $0`
 
 SCRIPT_NAME=`basename $0`
 WORKING_DIR=`pwd`
-
-if [ $# -lt 2 ]
+if [ $# -lt 1 ]
 then
-  echo "Usage: ./$SCRIPT_NAME tool_name tool_config.cfg"
-  echo "Example: ./$SCRIPT_NAME bwa  bwa.cfg"
+  echo "Usage: ./$SCRIPT_NAME tool_name"
+  echo "Example: ./$SCRIPT_NAME bwa"
   exit 1
 fi
-# Path relative to the root of this script
-data_source_config=$1/$2
-if [ ! -f $data_source_config ]
+tool_base=$1
+# Path to tool indexer base - relative to the root of this script
+if [ ! -d $tool_base ]
 then
-  echo "ERROR: $data_source_config file missing from `pwd`"
+  echo "ERROR: $tool_base directory missing from `pwd`"
   exit 1
 fi
-source ./$data_source_config
+cd $tool_base
+if [ ! -f $tool_base.cfg ]
+then
+  echo "ERROR $tool_base.cfg config file missing on `uname -n` under: `pwd`"
+  exit 1
+fi
+source ./$tool_base.cfg
 LOG_FILE="${LOGS_BASE}/$SCRIPT_NAME.$TOOL_VERSION.log"
-
-if [ ! -f $TOOL_DIR ]
-then
-  echo "ERROR $TOOL_DIR file missing on `uname -n`"
-  exit 1
-fi
 rm -rf $LOG_FILE
 touch $LOG_FILE
 date | tee -a $LOG_FILE
 echo "**********              *******************" | tee -a $LOG_FILE
 echo "Creating indexes for $TOOL_VERSION"| tee -a $LOG_FILE
 echo "**********  *******************************"| tee -a $LOG_FILE
+echo date | tee -a $LOG_FILE
 echo "Tool Version: $TOOL_VERSION"
 echo "Reference config files: $REFERENCE_FILE "
-
-if [ ! -d $INDEX_SCRIPT_BASE ]
-then
-   echo "ERROR: $INDEX_SCRIPT_BASE directory missing"
-   exit
-fi
-
-cd $INDEX_SCRIPT_BASE
-WORKING_DIR=`pwd`
-echo "Based directory for tool index scripts : $INDEX_SCRIPT_BASE"
-
-for tool in $ALIGN_INDEX_TOOLS
+## I need to store 
+for reference_config in REFERENCE_FILE
 do
-    TOOL_BASE=$WORKING_DIR/$tool
-    TOOL_CONFIG=$tool.cfg
-    [ ! -d $TOOL_BASE ] && continue
-    cd $TOOL_BASE
-    [ ! -f $REFERENCE_FILE ] && continue
-    [ ! -f $TOOL_CONFIG ] && continue
+    [ ! -f $reference_config ] && continue
+    echo "Indexind datasets in  : $reference_config"
+    
     for line in  `cat $REFERENCE_FILE`
     do
        IFS=', ' read -r -a fields <<< "$line"
        tool_name=${fields[0]}
-       [ ! -d $WORKING_DIR/$tool_name ] && continue
-       # get this tool version
-       source ./$TOOL_CONFIG
        organism=${fields[2]}
        dataset=${fields[3]}
        index_prefix=${fields[5]}
