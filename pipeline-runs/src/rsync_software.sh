bca87fa79282bb471b60b1e97ed0923526a53e9a
LucyHut <lucie.hutchins@yahoo.com>
2019-04-17 15:29:31 -0400

    Amazon S3 data migration

--- pipeline-runs/src/rsync_software.sh
+++ pipeline-runs/src/rsync_software.sh
@@ -11,49 +11,43 @@ fi
 #rsync_options="--links --ignore-errors"
 # make sure the connection is passwordless between the host
 # and the destination server - 
-#   https://www.centos.org/docs/5/html/5.1/Deployment_Guide/s3-openssh-dsa-key.html
+# https://www.centos.org/docs/5/html/5.1/Deployment_Guide/s3-openssh-dsa-key.html
 # A trailing slash on the source changes this behavior to avoid creating 
 # an additional directory level at the destination. You can think of a trailing / 
 # on a source as meaning "copy the contents of this directory" as opposed 
 # to "copy the directory by name",
 
-src_dir="/opt/software/"
-dest_server="ec2-user@ec2-18-232-77-165.compute-1.amazonaws.com"
-dest_dir="/opt/software"
 SCRIPT_NAME=`basename $0`
 
-#rsync_options="--links --ignore-errors"
-# Added --rsync-path='/usr/bin/sudo /usr/bin/rsync' 
-# to ensure that rsync was being run with elevated privileges remotely. 
-# This corrected the issue I was having:
-# rsync: send_files failed to open "/data/projects/DustinUpdike/Jesse_GLH-1/rsem/.Rhistory": Permission denied (13)
-
 #rsync_options=' -avz  --rsync-path="/usr/bin/sudo /usr/bin/rsync" --exclude=.snapshot'
-rsync_options=' -avz  --exclude=.snapshot'
+rsync_options=' -avz  --exclude=.snapshot --exclude=results --exclude=logs'
 
 
 #Check the number of arguments
-if [ $# -lt 3 ]
+if [ $# -lt 2 ]
 then
   echo ""
   echo "***********************************************"
   echo "Bad usage ---"
-  echo "Usage: ./$SCRIPT_NAME  LOCAL_DIR REMOTE_SERVER REMOTE_DIR"
-  echo "Example1: ./$SCRIPT_NAME  $src_dir $dest_server $dest_dir"
+  echo "Usage: ./$SCRIPT_NAME  LOCAL_DIR REMOTE_DIR"
+  echo "Example1: ./$SCRIPT_NAME  /data/scratch/ensembl-94/ /data/scratch/ensembl-94"
   echo ""
   echo "***********************************************"
   echo ""
   exit 1
 fi
 src_dir="$1"
-dest_server="$2"
-dest_dir="$3"
+dest_dir="$2"
+base_dir=`dirname $1`
+echo "Creating $base_dir"
+exit 0
+[ ! -d $base_dir ] && mkdir -p $base_dir
 
 ## rsync /opt/software
-rsync $rsync_options $src_dir $dest_server:$dest_dir 
+rsync $rsync_options $src_dir $dest_dir 
 if [ $? -ne 0 ]
 then
-   echo "Cmd: rsync $rsync_options $src_dir $dest_server:$dest_dir - FAILED"
+   echo "Cmd: rsync $rsync_options $src_dir $dest_dir - FAILED"
    exit 1
 fi
 exit 0
