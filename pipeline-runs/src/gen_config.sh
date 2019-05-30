44389d612698bdf6fd39226aab60ce6e5d0064f1
LucyHut <lucie.hutchins@yahoo.com>
2019-05-30 11:30:40 -0400

    refactoring

--- pipeline-runs/src/gen_config.sh
+++ pipeline-runs/src/gen_config.sh
@@ -22,30 +22,41 @@ ORGANISM=$4
 REF_DATABASE=$5
 REF_DATABASE_VERSION=$6
 # transcriptome_joined = cat  *.rna *.ncrna
-REF_DATASET=$7
+REF_DATASET=""
 # Set the format of R1 and R2
-READ1=$8
-READ2=$9
+READ1=""
+READ2=""
 ## use {} to access the value of argument number > 9
-CWL_SCRIPT=${10}
+CWL_SCRIPT=$7
+current_timestamp=${PROJECT_NAME}_$(date +%s)
 ## Where we will store the pipeline meta config file for each sample
 # name format sampleID.organism.pcf
-PCF_BASE=${11}
-PCF_PROJECT_BASE=$PCF_BASE/${PROJECT_TEAM_NAME}/${PROJECT_NAME}
+PCF_BASE=$8
+PCF_PROJECT_BASE=$PCF_BASE/${PROJECT_TEAM_NAME}/${PROJECT_NAME}/$current_timestamp
 ##We expect to find a json file for each sample under this path
 # flename format sampleID.organism.json
-JSON_BASE=${12}
-JSON_PROJECT_BASE=$JSON_BASE/${PROJECT_TEAM_NAME}/${PROJECT_NAME}
-READS_BASE=${13}/${PROJECT_TEAM_NAME}/${PROJECT_NAME}
+JSON_BASE=$9
+JSON_PROJECT_BASE=$JSON_BASE/${PROJECT_TEAM_NAME}/${PROJECT_NAME}/$current_timestamp
+READS_BASE=${10}/${PROJECT_TEAM_NAME}/${PROJECT_NAME}
 ## Setup path samples and design file 
 #DESIGN_FILE=${READS_BASE}/${PROJECT_NAME}.design.txt
-GIT_REPOS=${14}
-RESULTS_DIR_BASE=${15}/${PROJECT_TEAM_NAME}/${PROJECT_NAME}/results
-RESULTS_DIR=${RESULTS_DIR_BASE}/${PROJECT_NAME}_$(date +%s)
+GIT_REPOS=${11}
+RESULTS_DIR_BASE=${12}/${PROJECT_TEAM_NAME}/${PROJECT_NAME}/results
+RESULTS_DIR=${RESULTS_DIR_BASE}/$current_timestamp
+
+
 pipeline_config_base=${RESULTS_DIR}/cfgs
 pipeline_cfg_file=$pipeline_config_base/pipeline.cfg
 ## Setup path samples and design file 
-DESIGN_FILE=${16}
+DESIGN_FILE=${13}
+git_base=""
+
+if [[ $PIPELINE_PCF_BASE =~ .*($BIOCORE_PROJECTS_GIT_REPOS.*) ]] ; then
+    git_base="${BASH_REMATCH[1]}"
+fi
+
+echo "The git repos is: $git_base"
+
 
 GIT_REPOS_PCF_BASE=$GIT_REPOS/`basename $PCF_BASE`/${PROJECT_TEAM_NAME}/${PROJECT_NAME}
 GIT_REPOS_JSON_BASE=$GIT_REPOS/`basename $JSON_BASE`/${PROJECT_TEAM_NAME}/${PROJECT_NAME}
